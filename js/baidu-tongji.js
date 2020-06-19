7cf626a68f37f254e5c4b35bd8d8e12c9c63b73f
vincent <vincent@flins.com.cn>
2020-06-19 22:14:40 +0800

    Deployed b9db300 with MkDocs version: 1.1.2

---
+++ js/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c75e89ffa257be4a7dbf00277f3824ed";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
